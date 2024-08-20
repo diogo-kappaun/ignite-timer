c7dde74b6fa72159bd67c46ad7cac6c7a9f5b97d
diogo-kappaun <diogohkappaun@gmail.com>
2024-08-20 11:59:02 -0300

    :sparkles: feat: Create Link component

---
+++ src/components/Link.tsx
@@ -0,0 +1,21 @@
+import { NavLink } from 'react-router-dom'
+
+interface LinkProps {
+  link: string
+  title: string
+  children: any
+}
+
+export function Link({ link, title, children }: LinkProps) {
+  return (
+    <NavLink
+      to={link}
+      title={title}
+      className="flex h-12 w-12 items-center justify-center border-y-[3px] border-transparent text-gray-100 hover:border-b-green-500"
+    >
+      {({ isActive }) => (
+        <span className={isActive ? 'text-green-500' : ''}>{children}</span>
+      )}
+    </NavLink>
+  )
+}
