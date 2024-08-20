0ad817189e93cbce45ddf98b13d9f072f828b717
diogo-kappaun <diogohkappaun@gmail.com>
2024-08-20 11:59:53 -0300

    :sparkles: feat: Add header content

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,21 @@
+import { Scroll, Timer } from 'phosphor-react'
+import logoIgnite from '../assets/ignite-logo.svg'
+
+import { Link } from './Link'
+
 export function Header() {
   return (
-    <div>
-      <h1>Header</h1>
+    <div className="flex items-center justify-between">
+      <img src={logoIgnite} alt="" />
+
+      <nav className="flex gap-2">
+        <Link link="/" title="Timer">
+          <Timer size={24} />
+        </Link>
+        <Link link="/history" title="Histórico">
+          <Scroll size={24} />
+        </Link>
+      </nav>
     </div>
   )
 }
