9e5c00a4bbebc350bb859995d35762624bc574f8
diogo-kappaun <diogohkappaun@gmail.com>
2024-08-22 14:47:00 -0300

    :lipstick: fix: Remove status static color

--- src/components/Status.tsx
+++ src/components/Status.tsx
@@ -12,7 +12,7 @@ interface StatusProps {
 export default function Status({ children, statusColor }: StatusProps) {
   return (
     <span
-      className={`flex items-center gap-2 before:h-2 before:w-2 before:rounded-full before:bg-yellow-500 ${STATUS_COLORS[statusColor]}`}
+      className={`flex items-center gap-2 before:h-2 before:w-2 before:rounded-full ${STATUS_COLORS[statusColor]}`}
     >
       {children}
     </span>
